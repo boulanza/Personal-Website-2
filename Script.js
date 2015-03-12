8fd2fee5ece003b1c12da67e19e0acdaab0fd801
boulanza <boulanza@umich.edu>
2015-03-12 11:44:13 -0400

    more work on navbar stuff

--- Script.js
+++ Script.js
@@ -13,33 +13,56 @@ function nav_top() {
         $('nav').removeClass('stick');
         $('nav').addClass('unstick');
     }
+
+    nav_expand();
 }
 
 function nav_expand() {
-    //http://jsfiddle.net/Vy33z/4/
-    //this might help??
-    /*var $div = $('div');
-    var divleft = $div.position().left;
-    $('div').scroll(function() {
-        $('img').each(function() {
-            img = $(this);
-            imgleft = img.position().left;
-            if (imgleft >= divleft && imgleft + img.width() <= divleft + $div.width()) {
-                $(this).css({
-                    opacity: '1'
-                })
-                $(this).siblings('img').css({
-                    opacity: '0.2'
-                });
-                return false;
-            }
+    var top_of_screen = $(window).scrollTop();
+    var header1 = $('#header1').offset().top;
+    var header2 = $('#header2').offset().top;
+    var header3 = $('#header3').offset().top;
+
+    if (top_of_screen > header3) {
+        $('#sub1').css({
+            display: none
         });
-    })*/
+        $('#sub2').css({
+            display: none
+        });
+        $('#sub3').css({
+            display: inline
+        });
+    }
+    else if (top_of_screen > header2) {
+        $('#sub1').css({
+            display: none
+        });
+        $('#sub2').css({
+            display: inline
+        });
+        $('#sub3').css({
+            display: none
+        });
+    }
+    else if (top_of_screen > header1) {
+    alert("dklaj");
+        $('#sub1').css({
+            display: inline
+        });
+        $('#sub2').css({
+            display: none
+        });
+        $('#sub3').css({
+            display: none
+        });
+    }
 }
 
 $(function () {
     $(window).scroll(nav_top);
     nav_top();
+    nav_expand();
     $('nav').addClass('unstick');
 })
 
